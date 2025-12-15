4309aa987f8a4f5f3d64ca157d0263f916bdb1ad
Ausipainter <kjone795@evit.edu>
2025-12-15 14:03:14 -0700

    Update Main.py

--- Desktop Pet/Main.py
+++ Desktop Pet/Main.py
@@ -36,20 +36,36 @@ script_path = os.path.join(MAINDIR,"Pet.py")
 
 
 background_img = pygame.transform.scale(pygame.image.load(os.path.join(CONFIGDIR,"Background.png")), (WIDTH,HEIGHT))
+background_rect = background_img.get_rect()
 rightArrow_img = pygame.transform.scale(pygame.image.load(os.path.join(CONFIGDIR,"Arrow.png")), (100,100))
+rightArrow_rect = rightArrow_img.get_rect()
 leftArrow_img = pygame.transform.flip(pygame.transform.scale(pygame.image.load(os.path.join(CONFIGDIR,"Arrow.png")), (100,100)),True, False)
+leftArrow_rect = leftArrow_img.get_rect()
 petWindow_img = pygame.transform.scale(pygame.image.load(os.path.join(CONFIGDIR,"Pet.png")), (360,230))
 select_img = pygame.transform.scale(pygame.image.load(os.path.join(CONFIGDIR,"Select.png")), (300,80))
+select_rect = select_img.get_rect(topleft=(50,430))
+
 clock = pygame.time.Clock()
 running = True
+pet_process = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            sys.exit()
+            
+
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            mousex, mousey = pygame.mouse.get_pos()
+            mousex, mousey =event.pos 
+            mousepos = event.pos 
+            if select_rect.collidepoint(mousepos):
+                if pet_process:
+                    pet_process.kill()
+               
+                print("opened")
+                pet_process = subprocess.Popen(['python', script_path])
+                
+            
 
     screen.blit(background_img, (0,0))
     screen.blit(rightArrow_img,(240,270))
@@ -59,3 +75,8 @@ while running:
     pygame.display.flip()
     clock.tick(60)
     
+
+         
+if pet_process:
+    pet_process.kill()
+pygame.quit()
